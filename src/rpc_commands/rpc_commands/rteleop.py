4ef2dea743d3addcbefc2c24340c7cff6789142c
Teh Wei Sheng <wsjasonteh2003@gmail.com>
2024-07-15 15:15:53 +0700

    integrated into ROS package rpc_commands

--- src/rpc_commands/rpc_commands/rteleop.py
+++ src/rpc_commands/rpc_commands/rteleop.py
@@ -3,7 +3,7 @@ import subprocess
 import os
 
 # Define the base path for the movement scripts
-base_path = '/home/wshengggg/HWSpider/rpc_commands'
+base_path = '/home/wshengggg/HWSpider/src/rpc_commands/rpc_commands'
 
 # Define the mapping of keys to commands
 key_command_map = {
